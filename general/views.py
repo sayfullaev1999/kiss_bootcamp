e0f3c34f38f28094f09b2777db841ef16bd7a0fa
Zarif Bekturdiyev <9685440@gmail.com>
2021-01-26 00:42:04 +0500

    Template

--- general/views.py
+++ general/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
+from news.models import News
+from course.models import Course
+from project.models import Project
+from account.models import Mentor, User, Sponsor
 
 def home(request):
-    return render(request, 'home.html')
+    news = News.objects.all()[:3]
+    courses = Course.objects.all()[:4]
+    mentors = Mentor.objects.all()[:3]
+    projects = Project.objects.all()[:3]
+    context = {
+        'news':news,
+        'courses':courses,
+        'mentors':mentors,
+        'projects':projects,
+    }
+    return render(request, 'home.html', context=context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    users = User.objects.all()
+    projects = Project.objects.all()
+    sponsors = Sponsor.objects.all()
+    context = {
+        'users':users,
+        'projects':projects,
+        'sponsors':sponsors,
+    }
+    return render(request, 'about.html', context=context)
